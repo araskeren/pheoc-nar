b3f0f3e67779473c0cddec833adc7591b53ce884
Damis Bachtiar <damis.bachtiar@gmail.com>
2021-05-14 18:25:06 +0700

    add version 4

---
+++ app/Console/Commands/Analytic/DownloadV4.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Console\Commands\Analytic;
+
+use App\Area;
+use App\Exports\Nar\AnalyticExportV4;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Maatwebsite\Excel\Facades\Excel;
+
+class DownloadV4 extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'analytic:downloadV4 {--prov=}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Melakukan Download Analisa Nar Mingguan V4';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle():void
+    {
+        $prov = @$this->option('prov');
+        $this->info("Menjalankan Download Analisa Minggguan NAR - ".now()->format('Y-m-d H:i:s'));
+        $current_date = Carbon::now();
+        $path = "download/".$current_date->format('Y-m-d').'/nar';
+        if($prov){
+            $area = Area::select('id','name')->where(["name"=>$prov,"level"=>1])->first();
+            Excel::store(new AnalyticExportV4($area->id,$area->name),$path."/analytic_weekly_v2_".$area->name.".xlsx");
+        }else{
+            $area = Area::select('id','name')->where("level",1)->get();
+            $this->output->progressStart($area->count());
+            foreach($area as $i){
+                Excel::store(new AnalyticExportV4($i->id,$i->name),$path."/analytic_weekly_v2_".$i->name.".xlsx");
+                $this->output->progressAdvance();
+            }
+            $this->output->progressFinish();
+        }
+
+        $this->info("Berhasil Melakukan Download Analisa Minggguan NAR - ".now()->format('Y-m-d H:i:s'));
+    }
+}
---
+++ app/Exports/Nar/AnalyticExportV4.php
@@ -0,0 +1,218 @@
+<?php
+
+
+namespace App\Exports\Nar;
+
+
+use Illuminate\Support\Facades\DB;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithColumnFormatting;
+use Maatwebsite\Excel\Concerns\WithColumnWidths;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
+use Maatwebsite\Excel\Concerns\WithTitle;
+use PhpOffice\PhpSpreadsheet\Shared\Date;
+use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
+
+class AnalyticExportV4 implements FromCollection,WithTitle,WithMapping,WithHeadings,WithColumnFormatting,WithColumnWidths
+{
+    protected $provId;
+    protected $provName;
+
+    /**
+     * AnalyticExportV4 constructor.
+     * @param $provId
+     * @param $provName
+     */
+    public function __construct($provId, $provName)
+    {
+        $this->provId = $provId;
+        $this->provName = $provName;
+    }
+
+    public function collection()
+    {
+        $data = $this->getData($this->provName,$this->provId);
+        return collect($data);
+    }
+
+    public function headings(): array
+    {
+        return [
+            'TANGGAL PELAPORAN',
+            'PROVINSI',
+            'KABUPATEN/KOTA',
+            'Konfirmasi Harian',
+            'Konfirmasi Komulatif',
+            'Konfirmasi Komulatif 2021',
+            'Sembuh Harian',
+            'Sembuh Kumulatif',
+            'Meninggal Harian',
+            'Meninggal Kumulatif',
+            'Recv Rate (%)',
+            'CFR %',
+            'Kasus Aktif',
+            'Kasus Test Harian',
+            'Kasus Test Kum',
+            'Kasus Neg Harian',
+            'Kasus Neg Kum',
+            'Pos Rate (%)'
+        ];
+    }
+
+    public function map($data): array
+    {
+        if($data->tanggal >= '2020-12-27'){
+            return [
+                Date::stringToExcel($data->tanggal),
+                $this->provName,
+                $data->kabupaten,
+                $data->total_konfirm_harian,
+                $data->total_konfirm_kumulatif,
+                $data->total_konfirm_kumulatif_2021,
+                $data->total_sembuh_harian,
+                $data->total_sembuh_kumulatif,
+                $data->total_meninggal_harian,
+                $data->total_meninggal_kumulatif,
+                number_format($data->recovery_rate,2),
+                number_format($data->cfr,2),
+                $data->kasus_aktif,
+                $data->total_test_harian,
+                $data->total_test_harian_kumulatif,
+                $data->total_test_negatif,
+                $data->total_test_negatif_kumulatif,
+                number_format($data->positivity_rate,2)
+            ];
+        }
+    }
+
+    public function columnFormats(): array
+    {
+        return [
+            'A' => NumberFormat::FORMAT_DATE_DDMMYYYY
+        ];
+    }
+
+    public function columnWidths(): array
+    {
+        return [
+            'A' => 15,
+        ];
+    }
+
+    public function title(): string
+    {
+        return $this->provName;
+    }
+
+
+    private function getData($provName = "JAWA TENGAH",$provId = 32676)
+    {
+        return DB::select("
+        with recursive
+        cte_list_date as (
+            select CAST('2020-02-01' as DATE) as tanggal
+            union all
+            select CAST(tanggal + interval 1 day as DATE)
+            from cte_list_date
+            where tanggal < CURDATE()
+        ),
+        kab as (
+            select *
+            from area
+            where level = 2 and parent_id = " . $provId . "
+        ),
+        cte_kab_date as (
+            select *
+            from kab
+            cross join cte_list_date
+        ),
+        cte_konfirmasi_harian as (
+            select tanggal_lapor, kabupaten, count(*) as total_konfirm_harian
+            from line_list_nar
+            where tanggal_lapor <> '2000-00-00' and propinsi = '" . $provName . "'
+            group by tanggal_lapor, kabupaten
+        ),
+        cte_sembuh_harian as (
+            select tanggal_sembuh, kabupaten, count(*) as total_sembuh_harian
+            from line_list_nar
+            where tanggal_sembuh <> '2000-00-00' and propinsi = '" . $provName . "'
+            group by tanggal_sembuh, kabupaten
+        ),
+        cte_meninggal_harian as (
+            select tanggal_meninggal, kabupaten, count(*) as total_meninggal_harian
+            from line_list_nar
+            where tanggal_meninggal <> '2000-00-00' and propinsi = '" . $provName . "'
+            group by tanggal_meninggal, kabupaten
+        ),
+        cte_test_harian as (
+            select tgl,
+                   faskes_kabnm,
+                   sum(jml_spesimen_negatif +
+                       jml_spesimen_dalam_proses +
+                       jml_spesimen_inkonklusif +
+                       jml_spesimen_invalid +
+                       jml_spesimen_positiff) as total_test_harian
+            from analytic_spesiment_nar
+            where faskes_propnm = '" . $provName . "'
+              and faskes_kabnm is not null
+            group by tgl, faskes_kabnm
+        ),
+        cte_komulatif_2021 as (
+            select
+                kd.name                                             as kabupaten,
+                kd.tanggal                                          as tanggal_lapor,
+                sum(coalesce(total_konfirm_harian, 0))
+                    over (partition by kd.name order by kd.tanggal) as total
+            from cte_kab_date kd
+                     left join cte_konfirmasi_harian kh on kh.tanggal_lapor = kd.tanggal and kh.kabupaten = kd.name
+            where kd.tanggal >= '2020-12-27'
+        ),
+        cte_agg as (
+            select kd.name                                             as kabupaten,
+                   kd.tanggal,
+                   coalesce(total_konfirm_harian, 0)                   as total_konfirm_harian,
+                   coalesce(total_sembuh_harian, 0)                    as total_sembuh_harian,
+                   coalesce(total_meninggal_harian, 0)                 as total_meninggal_harian,
+                   coalesce(total_test_harian, 0)                      as total_test_harian,
+                   coalesce(jml_pasien_negatif, 0)                     as total_test_negatif,
+                   sum(coalesce(total_konfirm_harian, 0))
+                       over (partition by kd.name order by kd.tanggal) as total_konfirm_kumulatif,
+                   ck.total as total_konfirm_kumulatif_2021,
+                   sum(coalesce(total_sembuh_harian, 0))
+                       over (partition by kd.name order by kd.tanggal) as total_sembuh_kumulatif,
+                   sum(coalesce(total_meninggal_harian, 0))
+                       over (partition by kd.name order by kd.tanggal) as total_meninggal_kumulatif,
+                   sum(coalesce(total_test_harian, 0))
+                       over (partition by kd.name order by kd.tanggal) as total_test_harian_kumulatif,
+                   sum(coalesce(jml_pasien_negatif, 0))
+                       over (partition by kd.name order by kd.tanggal) as total_test_negatif_kumulatif
+            from cte_kab_date kd
+                     left join cte_konfirmasi_harian kk
+                               on kk.tanggal_lapor = kd.tanggal and
+                                  kk.kabupaten = kd.name
+                     left join cte_komulatif_2021 ck
+                               on ck.tanggal_lapor = kd.tanggal and
+                                  ck.kabupaten = kd.name
+                     left join cte_sembuh_harian sk
+                               on sk.tanggal_sembuh = kd.tanggal and
+                                  sk.kabupaten = kd.name
+                     left join cte_meninggal_harian mk
+                               on mk.tanggal_meninggal = kd.tanggal and
+                                  mk.kabupaten = kd.name
+                     left join cte_test_harian tk
+                               on tk.tgl = kd.tanggal and
+                                  tk.faskes_kabnm = kd.name
+                     left join analytic_patient_nar apn on apn.tgl = kd.tanggal and apn.faskes_kabnm = kd.name
+            order by kd.name, kd.tanggal
+        )
+    select *,
+           total_sembuh_kumulatif / total_konfirm_kumulatif * 100                         as recovery_rate,
+           total_meninggal_kumulatif / total_konfirm_kumulatif * 100                      as cfr,
+           total_konfirm_kumulatif - (total_sembuh_kumulatif + total_meninggal_kumulatif) as kasus_aktif,
+           (total_konfirm_kumulatif_2021 / (total_konfirm_kumulatif_2021+total_test_negatif_kumulatif)) * 100                    as positivity_rate
+    from cte_agg
+    where tanggal >= '2020-12-27';
+        ");
+    }
+}
