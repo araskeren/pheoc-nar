c151d77c95beaad19f799c41b1906cf51bf8ca9b
DESKTOP-MV9TU5O\damis <damis.bachtiar@gmail.com>
2021-06-17 22:59:42 +0700

    cleaning line list

--- app/Console/Commands/CleaningLineList.php
+++ app/Console/Commands/CleaningLineList.php
@@ -12,7 +12,7 @@ class CleaningLineList extends Command
      *
      * @var string
      */
-    protected $signature = 'cleaning:linelist';
+    protected $signature = 'cleaning:linelist {--part=}';
 
     /**
      * The console command description.
@@ -38,66 +38,73 @@ class CleaningLineList extends Command
      */
     public function handle()
     {
+        $part = @$this->option('part');
         $this->info("Menjalankan Cleaning Line List - ".now()->format('Y-m-d H:i:s'));
 
         $this->info("");
-        $this->info("Cleaning Tanggal Lapor");
-        $lapor = DB::table("line_list_nar")->select("id","tanggal_lapor")->whereRaw('length(tanggal_lapor) < 10 and tanggal_lapor != "0/0/0"')->get();
-        $this->output->progressStart($lapor->count());
-        foreach ($lapor as $i){
-            $split = explode("/",$i->tanggal_lapor);
-            $date = (int) $split[0];
-            $date = ($date<=9)?"0".$date:$date;
-            $month = (int) $split[1];
-            $month = ($month<=9)?"0".$month:$month;
-            $year = $split[2];
-            $tanggalLapor = $date."/".$month."/".$year;
-            DB::table('line_list_nar')->where('id', $i->id)
-                ->update(['tanggal_lapor' => $tanggalLapor]);
-            $this->output->progressAdvance();
+        if($part == 0 or $part == 1){
+            $this->info("Cleaning Tanggal Lapor");
+            $lapor = DB::table("line_list_nar")->select("id","tanggal_lapor")->whereRaw('length(tanggal_lapor) < 10 and tanggal_lapor != "0/0/0"')->get();
+            $this->output->progressStart($lapor->count());
+            foreach ($lapor as $i){
+                $split = explode("/",$i->tanggal_lapor);
+                $date = (int) $split[0];
+                $date = ($date<=9)?"0".$date:$date;
+                $month = (int) $split[1];
+                $month = ($month<=9)?"0".$month:$month;
+                $year = $split[2];
+                $tanggalLapor = $date."/".$month."/".$year;
+                DB::table('line_list_nar')->where('id', $i->id)
+                    ->update(['tanggal_lapor' => $tanggalLapor]);
+                $this->output->progressAdvance();
+            }
+            $this->output->progressFinish();
+            $this->info("End Cleaning Tanggal Lapor");
         }
-        $this->output->progressFinish();
-        $this->info("End Cleaning Tanggal Lapor");
 
-        $meninggal = DB::table("line_list_nar")->select("id","tanggal_meninggal")->whereRaw('length(tanggal_meninggal) < 10 and tanggal_meninggal != "0/0/0"')->get();
+        if($part == 0 or $part == 2){
+            $meninggal = DB::table("line_list_nar")->select("id","tanggal_meninggal")->whereRaw('length(tanggal_meninggal) < 10 and tanggal_meninggal != "0/0/0"')->get();
 
-        $this->info("");
-        $this->info("Cleaning Meninggal");
-        $this->output->progressStart($meninggal->count());
-        foreach ($meninggal as $i){
-            $split = explode("/",$i->tanggal_meninggal);
-            $date = (int) $split[0];
-            $date = ($date<=9)?"0".$date:$date;
-            $month = (int) $split[1];
-            $month = ($month<=9)?"0".$month:$month;
-            $year = $split[2];
-            $tanggalMeninggal = $date."/".$month."/".$year;
-            DB::table('line_list_nar')->where('id', $i->id)
-                ->update(['tanggal_meninggal' => $tanggalMeninggal]);
-            $this->output->progressAdvance();
+            $this->info("");
+            $this->info("Cleaning Meninggal");
+            $this->output->progressStart($meninggal->count());
+            foreach ($meninggal as $i){
+                $split = explode("/",$i->tanggal_meninggal);
+                $date = (int) $split[0];
+                $date = ($date<=9)?"0".$date:$date;
+                $month = (int) $split[1];
+                $month = ($month<=9)?"0".$month:$month;
+                $year = $split[2];
+                $tanggalMeninggal = $date."/".$month."/".$year;
+                DB::table('line_list_nar')->where('id', $i->id)
+                    ->update(['tanggal_meninggal' => $tanggalMeninggal]);
+                $this->output->progressAdvance();
+            }
+            $this->output->progressFinish();
+            $this->info("End Cleaning Meninggal");
         }
-        $this->output->progressFinish();
-        $this->info("End Cleaning Meninggal");
 
-        $sembuh = DB::table("line_list_nar")->select("id","tanggal_sembuh")->whereRaw('length(tanggal_sembuh) < 10 and tanggal_sembuh != "0/0/0"')->get();
+        if($part == 0 or $part == 3){
+            $sembuh = DB::table("line_list_nar")->select("id","tanggal_sembuh")->whereRaw('length(tanggal_sembuh) < 10 and tanggal_sembuh != "0/0/0"')->get();
 
-        $this->info("");
-        $this->info("Cleaning Sembuh");
-        $this->output->progressStart($sembuh->count());
-        foreach ($sembuh as $i){
-            $split = explode("/",$i->tanggal_sembuh);
-            $date = (int) $split[0];
-            $date = ($date<=9)?"0".$date:$date;
-            $month = (int) $split[1];
-            $month = ($month<=9)?"0".$month:$month;
-            $year = $split[2];
-            $tanggalSembuh = $date."/".$month."/".$year;
-            DB::table('line_list_nar')->where('id', $i->id)
-                ->update(['tanggal_sembuh' => $tanggalSembuh]);
-            $this->output->progressAdvance();
+            $this->info("");
+            $this->info("Cleaning Sembuh");
+            $this->output->progressStart($sembuh->count());
+            foreach ($sembuh as $i){
+                $split = explode("/",$i->tanggal_sembuh);
+                $date = (int) $split[0];
+                $date = ($date<=9)?"0".$date:$date;
+                $month = (int) $split[1];
+                $month = ($month<=9)?"0".$month:$month;
+                $year = $split[2];
+                $tanggalSembuh = $date."/".$month."/".$year;
+                DB::table('line_list_nar')->where('id', $i->id)
+                    ->update(['tanggal_sembuh' => $tanggalSembuh]);
+                $this->output->progressAdvance();
+            }
+            $this->output->progressFinish();
+            $this->info("End Cleaning Sembuh");
         }
-        $this->output->progressFinish();
-        $this->info("End Cleaning Sembuh");
 
         $this->info("Berhasil menjalankan Cleaning Line List - ".now()->format('Y-m-d H:i:s'));
     }
